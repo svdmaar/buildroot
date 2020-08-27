16e685532b64ea813c4562c519918bb511e9daa2
Neeraj Deshpande <n.deshpande@metrological.com>
2020-08-28 01:19:23 +0530

    Added support for arris board.

--- package/wpebackend-rdk/wpebackend-rdk.mk
+++ package/wpebackend-rdk/wpebackend-rdk.mk
@@ -7,7 +7,7 @@
 WPEBACKEND_RDK_VERSION = 3ec8dfd1a1f1cede256fd5de0a63a8c6b6a31ffa
 WPEBACKEND_RDK_SITE = $(call github,WebPlatformForEmbedded,WPEBackend-rdk,$(WPEBACKEND_RDK_VERSION))
 WPEBACKEND_RDK_INSTALL_STAGING = YES
-WPEBACKEND_RDK_DEPENDENCIES = libwpe libglib2 rpi-userland libinput
+WPEBACKEND_RDK_DEPENDENCIES = libwpe libglib2 libinput
 
 ifeq ($(BR2_PACKAGE_LIBXKBCOMMON),y)
 WPEBACKEND_RDK_DEPENDENCIES += libxkbcommon
