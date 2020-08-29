3c1d45c677e912b1f3f218456b3b18f53286533d
Artur Gebicz <a.gebicz@metrological.com>
2020-08-29 13:33:47 +0200

    [netflix52] Bump to include gap handling and fade

--- package/netflix52/netflix52.mk
+++ package/netflix52/netflix52.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX52_VERSION = 52f0457d92ad395ef5ef8957d3f40a88301ae6ff
+NETFLIX52_VERSION = 28638b48fbcdda0bd1fd8853209cea15f1831f04
 NETFLIX52_SITE = git@github.com:Metrological/netflix.git
 NETFLIX52_SITE_METHOD = git
 NETFLIX52_LICENSE = PROPRIETARY
